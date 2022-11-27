4abf60ec3a89e3a66eee599f25dda4b386b05478
SimonaytesYan <yandexman228@gmail.com>
2022-11-27 17:57:58 +0300

    Fix add pow in recursive descent + fix codestyle on it

--- Differentiator/Libs/ResursiveDescent/RecursiveDescent.cpp
+++ Differentiator/Libs/ResursiveDescent/RecursiveDescent.cpp
@@ -2,14 +2,17 @@
 
 #include "RecursiveDescent.h"
 
+#define DEBUG
+
 //----------------------------
-//G ::= E ';'
-//E ::= T{['+','-']T}*
-//T ::= P{['*','/']P}*
-//O ::= {"**" | "sin" | "cos" | "log"}?P
-//P ::= '('E')' | V
-//V ::= ['a'-'z'] | N
-//N ::= ['0'-'9']+
+//G   ::= E ';'
+//E   ::= T{['+','-']T}*
+//T   ::= P{['*','/']P}*
+//O   ::= {"sin" | "cos" | "log"}?POW
+//POW ::= P {"**"P}*
+//P   ::= '('E')' | V
+//V   ::= ['a'-'z'] | N
+//N   ::= ['0'-'9']+
 //----------------------------
 
 //----------------------------
@@ -17,19 +20,19 @@
 //-: -5; +7; -19*7; x + u15; 17l; x + y - ; kl; A
 //----------------------------
 
-const char* s = nullptr;
+static Node* GetE(const char** s);
 
-Node* GetE();
+static Node* GetT(const char** s);
 
-static Node* GetT();
+static Node* GetP(const char** s);
 
-static Node* GetP();
+static Node* GetV(const char** s);
 
-static Node* GetV();
+static Node* GetN(const char** s);
 
-static Node* GetN();
+static Node* GetO(const char** s);
 
-static Node* GetO();
+static Node* GetPow(const char** s);
 
 Node* CreateNodeWithChild_Op(Node* left_node, Node* right_node, OPER_TYPES op);
 
@@ -44,24 +47,30 @@ Node* CreateNodeWithChild_Op(Node* left_node, Node* right_node, OPER_TYPES op)
 
 Node* GetG(const char* str)
 {
-    s = str;
+    #ifdef DEBUG
+        printf("(G) s = <%s>\n", str);
+    #endif
 
-    Node* val = GetE();
-    assert(*s == ';');
+    Node* val = GetE(&str);
+    assert(*str == ';');
 
     return val;
 }
 
-Node* GetE()
+Node* GetE(const char** s)
 {
-    Node* val = GetT();
+    #ifdef DEBUG
+        printf("(E) s = <%s>\n", *s);
+    #endif
 
-    while (*s == '+' || *s == '-')
+    Node* val = GetT(s);
+
+    while (**s == '+' || **s == '-')
     {
-        char op = *s;
-        s++;
+        char op = **s;
+        (*s)++;
 
-        Node* right_node = GetT();
+        Node* right_node = GetT(s);
 
         if (op == '+')
             val = CreateNodeWithChild_Op(val, right_node, OP_PLUS);
@@ -69,18 +78,25 @@ Node* GetE()
             val = CreateNodeWithChild_Op(val, right_node, OP_SUB);
     }
 
+    #ifdef DEBUG
+        printf("end E\n");
+    #endif
     return val;
 }
 
-Node* GetT()
+Node* GetT(const char** s)
 {
-    Node* val = GetO();
-    while (*s == '*' || *s == '/')
-    {
-        char op = *s;
-        s++;
+    #ifdef DEBUG
+        printf("(T) s = <%s>\n", *s);
+    #endif
 
-        Node* right_node = GetO();
+    Node* val = GetO(s);
+    while (**s == '*' || **s == '/')
+    {
+        char op = **s;
+        (*s)++;
+
+        Node* right_node = GetO(s);
 
         if (op == '*')
             val = CreateNodeWithChild_Op(val, right_node, OP_MUL);
@@ -88,96 +104,139 @@ Node* GetT()
             val = CreateNodeWithChild_Op(val, right_node, OP_DIV);
     }
 
+    #ifdef DEBUG
+        printf("end T\n");
+    #endif
+
     return val;
 }
 
-Node* GetO()
+Node* GetO(const char** s)
 {
-    const char* old_s = s;
+    #ifdef DEBUG
+        printf("(O) s = <%s>\n", *s);
+    #endif
+
+    const char* old_s = *s;
 
     Node* node = nullptr;
-    if (!strncmp(s, "**", 2))
+    if (!strncmp(*s, "sin", 3))
     {
-        node = NodeCtorOp(OP_POW);
-        s += 2;
-        R(node) = GetP();
-    }
-    else if (!strncmp(s, "sin", 3))
-    {
-        printf("(O) go to sin\n");
         node = NodeCtorOp(OP_SIN);
-        s += 3;
-        R(node) = GetP();
+        (*s) += 3;
+        R(node) = GetPow(s);
     }
-    else if (!strncmp(s, "cos", 3))
+    else if (!strncmp(*s, "cos", 3))
     {
-        printf("(O) go to cos\n");
         node = NodeCtorOp(OP_COS);
-        s += 3;
-        R(node) = GetP();
+        (*s) += 3;
+        R(node) = GetPow(s);
     }
-    else if (!strncmp(s, "log", 3))
+    else if (!strncmp(*s, "log", 3))
     {
-        printf("(O) go to log\n");
         node = NodeCtorOp(OP_LOG);
-        s += 3;
-        R(node) = GetP();
+        (*s) += 3;
+        R(node) = GetPow(s);
     }
     else
-    {
-        printf("(O) go to P\n");
-        node = GetP();
-    }
+        node = GetPow(s);
 
-    assert(s != old_s);
+    assert(*s != old_s);
+
+    #ifdef DEBUG
+        printf("end O\n");
+    #endif
+    
+    return node;
+}
+
+Node* GetPow(const char** s)
+{
+    #ifdef DEBUG
+        printf("(POW) s = <%s>", *s);
+    #endif
+    Node* node = GetP(s);
+
+    while (!strncmp(*s, "**", 2))
+    {
+        (*s)+=2;
+        Node* node_right = GetP(s);
+        node = CreateNodeWithChild_Op(node, node_right, OP_POW);
+    }
+    
+    return node;
+}
+
+Node* GetP(const char** s)
+{
+    #ifdef DEBUG
+        printf("(P) s = <%s>\n", *s);
+    #endif
+
+    Node* val = 0;
+    if (**s == '(')
+    {
+        (*s)++;
+        val = GetE(s);
+        assert(**s == ')');
+        (*s)++;
+    }
+    else
+        val = GetV(s);
+
+    #ifdef DEBUG
+        printf("end P\n");
+    #endif
+
+    return val;
+}
+
+Node* GetV(const char** s)
+{
+    #ifdef DEBUG
+        printf("(V) s = <%s>\n", *s);
+    #endif
+
+    Node* node = nullptr;
+    if ('a' <= **s && **s <= 'z')
+    {
+        char* val = (char*)calloc(2, sizeof(char));
+        val[0] = **s;
+        (*s)++;
+
+        node = NodeCtorVar(val);
+    }
+    else
+        node = GetN(s);
+    
+    #ifdef DEBUG
+        printf("end V\n");
+    #endif
 
     return node;
 }
 
-Node* GetP()
+Node* GetN(const char** s)
 {
-    Node* val = 0;
-    if (*s == '(')
-    {
-        s++;
-        val = GetE();
-        assert(*s == ')');
-        s++;
-    }
-    else
-        val = GetV();
+    #ifdef DEBUG
+        printf("(N) s = <%s>\n", *s);
+    #endif
 
-    return val;
-}
-
-Node* GetV()
-{
-    if ('a' <= *s && *s <= 'z')
-    {
-        char* val = (char*)calloc(2, sizeof(char));
-        val[0] = *s;
-        s++;
-        printf("add var <%s>\n", val);
-
-        return NodeCtorVar(val);
-    }
-    else
-        return GetN();
-}
-
-Node* GetN()
-{
     int val = 0;
-    const char* old_s = s;
+    const char* old_s = *s;
 
-    while('0' <= *s && *s <= '9')
+    while('0' <= **s && **s <= '9')
     {
-        val = val*10 + *s - '0';
-        s++;
+        val = val*10 + **s - '0';
+        (*s)++;
     }
 
-    assert(s != old_s);
+    assert(*s != old_s);
     Node* new_node = NodeCtorNum(val);
+
+    #ifdef DEBUG
+        printf("end N\n");
+    #endif
 
     return new_node;
 }
